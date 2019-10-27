285e14646f5a03ee80ca25afafd9f7ee16ecad2b
nhovay8 <nhovay8@gmail.com>
2019-10-27 23:47:28 +0700

    add 2_deploy_contract for deploy

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.5.11;
 
+// implements on this documentation https://eips.ethereum.org/EIPS/eip-20
 contract DappToken {
     //Constructor
     //set the total number of tokens
     //Read the total number of tokens
+    uint256 public totalSupply; //state variables
+    
+    constructor() public {
+        totalSupply = 1000000;
+    }
 }
