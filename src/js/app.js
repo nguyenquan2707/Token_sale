41c7b3e44f26136815d682cea7021d951b8de986
nhovay8 <nhovay8@gmail.com>
2019-11-04 23:23:09 +0700

    remove web3 library

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log('load...');
+import { isUserEthereumAddressInBloom } from "web3-utils";
+import Web3 from "web3";
+
+App = {
+    init: function() {
+        return App.initWeb3();
+    },
+    initWeb3: function() {
+        if(typeof web3 !== 'undefined') {
+           App.provider = web3.currentProvider;
+           ethereum.enable();
+           web3 = new Web3(web3.currentProvider);
+        } else {
+            
+        }
+    }
+};
+
+$(function(){
+    $(window).load(function(){
+        App.init();
+    });
+});
