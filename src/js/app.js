3dc033e442e7cd4bccb1c6d486f5dc5542c06816
nhovay8 <nhovay8@gmail.com>
2019-11-04 23:38:18 +0700

    config web3 provider in App.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,20 @@
-import { isUserEthereumAddressInBloom } from "web3-utils";
-import Web3 from "web3";
-
 App = {
+    web3Provider: null,
     init: function() {
+        console.log('Loanding init function...........');
         return App.initWeb3();
     },
     initWeb3: function() {
         if(typeof web3 !== 'undefined') {
-           App.provider = web3.currentProvider;
+            //if web3 is provided by metamask
+           App.web3Provider = web3.currentProvider;
            ethereum.enable();
            web3 = new Web3(web3.currentProvider);
         } else {
-            
+            // Specify default instance of web3 if no web3 is provided
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            ethereum.enable();
+            web3 = new Web3(App.web3Provider);
         }
     }
 };
