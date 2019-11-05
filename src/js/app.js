6f02a62ca1947db8b033db44ddd35a20e9c16ea4
nhovay8 <nhovay8@gmail.com>
2019-11-05 21:44:58 +0700

    load data from blockchain and show in app

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 App = {
     web3Provider: null,
+    contracts: {},
+    account: '0x0',
     init: function() {
         console.log('Loanding init function...........');
         return App.initWeb3();
@@ -16,6 +18,44 @@ App = {
             ethereum.enable();
             web3 = new Web3(App.web3Provider);
         }
+        return App.initContract();
+    },
+
+    initContract: function() {
+        $.getJSON("DappTokenSale.json", function(dappTokenSale) {
+            App.contracts.DappTokenSale = TruffleContract(dappTokenSale);//TruffleContract allow us to read the contract
+            App.contracts.DappTokenSale.setProvider(App.web3Provider);
+            App.contracts.DappTokenSale.deployed().then(function(instance){
+                console.log('DappTokenSale address: ' + instance.address);
+            });
+        }).done(function(){
+            $.getJSON("DappToken.json",function(dappToken){
+                App.contracts.DappToken = TruffleContract(dappToken);//TruffleContract allow us to read the contract
+                App.contracts.DappToken.setProvider(App.web3Provider);
+                App.contracts.DappToken.deployed().then(function(dappToken){
+                    console.log('DappToken address: ' + dappToken.address);
+                });
+            });
+        });
+        App.render();
+    },
+
+    render: function() {
+        var loader = $('#loader');
+        var content = $('#content');
+        var accountAddress = $('#accountAddress');
+
+        loader.show();
+        content.hide();
+        //load account data
+        web3.eth.getCoinbase(function(error, account){
+            if(error === null) {
+                App.account= account;
+                accountAddress.html("your account: " + account);
+                console.log("your account " + account);
+            }
+        });
+
     }
 };
 
